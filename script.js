bece25de6ca86f5ae426a2abde618a4f944249a9
bitschrauber <78683158+bitschrauber@users.noreply.github.com>
2022-06-10 14:09:10 +0200

    Added player. Shows active player-

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 /**
  * 
  */
- 
+let player = 1
+
  function btnClicked(id) {
 	//let iid = 'square'+id
 	console.log(id);
 	document.getElementById(id).innerHTML = 'X';
+	player=player==1?player=0:player=1;
+	document.getElementById("player").innerHTML="Player"+player;
 }
