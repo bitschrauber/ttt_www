3c2c81633db2b462311d983e9723b055c5b94694
bitschrauber <78683158+bitschrauber@users.noreply.github.com>
2022-06-10 21:25:21 +0200

    Put <script> at the end of <body>, so querySelectorAll works now function btnClicked() now withot getElementById() ID="square1" ... removed.

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 /**
- * bitschrauber 2022
+ * bitschrauber 2022 #row1 > button:nth-child(1)
  */
+//console.log("Entered script");
+ 
+btns = document.querySelectorAll("button");
+//console.log(btns.length);
+
+btns.forEach ((btn)=>{
+	btn.addEventListener ("click", btnClicked)
+	//console.log('added')
+})
+
 let player = 1
 
- function btnClicked(id) {
-	//let iid = 'square'+id
-	console.log(id);
-	document.getElementById(id).innerHTML = 'X';
+ function btnClicked() {
+	this.innerHTML=player==1?'X':'O';
 	player=player==1?player=2:player=1;
 	document.getElementById("player").innerHTML="Player "+player;
 }
