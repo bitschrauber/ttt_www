7cb656caa56cec97081d6500d5d76f9473007f47
bitschrauber <mgropp@web.de>
2022-06-10 15:04:14 +0200

    Show active player

--- script.js
+++ script.js
@@ -7,6 +7,6 @@ let player = 1
 	//let iid = 'square'+id
 	console.log(id);
 	document.getElementById(id).innerHTML = 'X';
-	player=player==1?player=0:player=1;
-	document.getElementById("player").innerHTML="Player"+player;
+	player=player==1?player=2:player=1;
+	document.getElementById("player").innerHTML="Player "+player;
 }
