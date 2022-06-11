18644e0cb5727d587ecdce266e17b4664edff5dc
bitschrauber <78683158+bitschrauber@users.noreply.github.com>
2022-06-11 22:16:49 +0200

    Added some function to set a mark X or O into the field and manage player

--- script.js
+++ script.js
@@ -13,8 +13,27 @@ btns.forEach ((btn)=>{
 
 let player = 1
 
- function btnClicked() {
-	this.innerHTML=player==1?'X':'O';
-	player=player==1?player=2:player=1;
+function isEmptyField(e){
+	console.log(e)
+	return !((e=="X")||(e=="O"));
+}
+
+function setMark(player){
+	return (player==1?'X':'O');
+}
+
+function togglePlayer(player){
+	player=(player==1?player=2:player=1);
+}
+
+function announcePlayer(player){
 	document.getElementById("player").innerHTML="Player "+player;
+}
+
+function btnClicked(){	
+	if (isEmptyField(this.innerHTML)){
+		this.innerHTML=setMark(player);
+		togglePlayer(player);
+		announcePlayer(player)
+	} else {return}
 }
